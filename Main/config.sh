fd182d7e9c35531ec1ee3a06ddfeed9ebdaed563
Las-Mohamed <128605474+Las-Mohamed@users.noreply.github.com>
2023-07-12 09:58:44 +0200

    Update config.sh

--- Main/config.sh
+++ Main/config.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 terraform output -raw Cle_privee > id_rsa
 sudo chmod 400 id_rsa
-sudo echo -e "[masters]\n\nmaster ansible_host=IPIP_manager ansible_user=momo\nmaster ansible_ssh_private_key_file=id_rsa\n\n[workers]\n\nworker0 ansible_host=$(IPIP_worker_0) ansible_user=momo\nworker0 ansible_ssh_private_key_file=id_rsa\n\nworker1 ansible_host=$IPIP_worker_0 ansible_user=momo\nworker1 ansible_ssh_private_key_file=id_rsa" > inventaire.ini
+sudo echo -e "[masters]\n\nmaster ansible_host=$IP_manager ansible_user=momo\nmaster ansible_ssh_private_key_file=id_rsa\n\n[workers]\n\nworker0 ansible_host=$(IPIP_worker_0) ansible_user=momo\nworker0 ansible_ssh_private_key_file=id_rsa\n\nworker1 ansible_host=$IPIP_worker_0 ansible_user=momo\nworker1 ansible_ssh_private_key_file=id_rsa" > inventaire.ini
